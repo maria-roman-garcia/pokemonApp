6e6f0321b4aace50bb54775a5bb720079074e885
María Roman Garcia <maria.roman@delonia.es>
2021-01-21 17:40:57 +0100

    pagination to menu

--- src/App.js
+++ src/App.js
@@ -11,22 +11,12 @@ function App() {
 
   //Context
   const [contextInfo, setContextInfo] = useState({
-    screenWidth: window.innerWidth,
     paginationSelected: 1,
     count: 1118, //The total number of resources available from this API. (Int)
     pokemonResults: [], //A list of named API resources.
     pokemonDetailed: undefined,
     loading: true,
   });
-
-  //Calculate the screen width
-  useEffect(() => {
-    const updateWindowDimensions = () => {
-      setContextInfo({ ...contextInfo, screenWidth: window.innerWidth });
-    };
-    window.addEventListener("resize", updateWindowDimensions);
-    return () => window.removeEventListener("resize", updateWindowDimensions);
-  }, []);
 
   return (
     <div className="App">
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,28 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Context from '../Context/Context';
 //reactstrap
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
-const Menu=()=>{
+const Menu = () => {
 
     // context
     const contextComponent = useContext(Context);
     const contextComponentValue = contextComponent.contextInfo;
     const setContextComponentValue = contextComponent.setContextInfo;
 
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    //Calculate the screen width
+    useEffect(() => {
+        const updateWindowDimensions = () => {
+            setScreenWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", updateWindowDimensions);
+        return () => window.removeEventListener("resize", updateWindowDimensions);
+    }, []);
+
     const totalPagination = Math.ceil(contextComponentValue.count / 20);
-    
+
     const paginationNum = () => {
         const paginationSelected = contextComponentValue.paginationSelected;
         const withEachCell = 45;
         let arrowsSize = withEachCell * 4;
-        let widthScreen = contextComponentValue.screenWidth;
-        if (contextComponentValue.screenWidth > 1402) {
+        let widthScreen = screenWidth;
+        if (screenWidth > 1402) {
             widthScreen = 1402;
         }
         let numberPaginationOptions = Math.floor((widthScreen - arrowsSize) / withEachCell);
         let minVal = 0;
         let maxVal = 0;
+        if (numberPaginationOptions > totalPagination) {
+            numberPaginationOptions = totalPagination;
+        }
         if (paginationSelected - Math.floor(numberPaginationOptions / 2) < 1) {
             minVal = 1;
             maxVal = numberPaginationOptions;
@@ -36,7 +49,7 @@ const Menu=()=>{
         let toReturn = [];
         for (let i = minVal; i <= maxVal; i++) {
             toReturn.push(
-                <PaginationItem className={paginationSelected === i ? "active" : undefined} onClick={() => setContextComponentValue({...contextComponentValue, paginationSelected: i})} key={"paginationItem_" + i}>
+                <PaginationItem className={paginationSelected === i ? "active" : undefined} onClick={() => setContextComponentValue({ ...contextComponentValue, paginationSelected: i })} key={"paginationItem_" + i}>
                     <PaginationLink href="#">
                         {i}
                     </PaginationLink>
@@ -46,22 +59,22 @@ const Menu=()=>{
         return toReturn;
     }
 
-    return(
+    return (
         <div className="Menu">
             <div className="row justifyCenter">
-                <Pagination aria-label="Page navigation example" style={{display: "flex", justifyContent: "center"}}>
+                <Pagination aria-label="Page navigation example" style={{ display: "flex", justifyContent: "center" }}>
                     <PaginationItem disabled={contextComponentValue.paginationSelected === 1}>
-                        <PaginationLink first onClick={() => setContextComponentValue({...contextComponentValue, paginationSelected: 1})} />
+                        <PaginationLink first onClick={() => setContextComponentValue({ ...contextComponentValue, paginationSelected: 1 })} />
                     </PaginationItem>
                     <PaginationItem disabled={contextComponentValue.paginationSelected === 1}>
-                        <PaginationLink previous onClick={() => setContextComponentValue({...contextComponentValue, paginationSelected: contextComponentValue.paginationSelected -1})}/>
+                        <PaginationLink previous onClick={() => setContextComponentValue({ ...contextComponentValue, paginationSelected: contextComponentValue.paginationSelected - 1 })} />
                     </PaginationItem>
                     {paginationNum()}
                     <PaginationItem disabled={contextComponentValue.paginationSelected === totalPagination}>
-                        <PaginationLink next onClick={() => setContextComponentValue({...contextComponentValue, paginationSelected: contextComponentValue.paginationSelected +1})}/>
+                        <PaginationLink next onClick={() => setContextComponentValue({ ...contextComponentValue, paginationSelected: contextComponentValue.paginationSelected + 1 })} />
                     </PaginationItem>
                     <PaginationItem disabled={contextComponentValue.paginationSelected === totalPagination}>
-                        <PaginationLink last onClick={() => setContextComponentValue({...contextComponentValue, paginationSelected: totalPagination})}/>
+                        <PaginationLink last onClick={() => setContextComponentValue({ ...contextComponentValue, paginationSelected: totalPagination })} />
                     </PaginationItem>
                 </Pagination>
             </div>
